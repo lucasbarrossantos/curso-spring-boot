b38e14a7e611d7212511720a69a2132e6985f799
lucasbarrossantos <lucas-barros28@hotmail.com>
2021-12-29 16:01:47 -0300

    Aula 05 Criacao do service e salvando os dados no H2

--- compra/src/main/java/com/mscompra/model/Pedido.java
+++ compra/src/main/java/com/mscompra/model/Pedido.java
@@ -9,6 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -18,8 +22,12 @@ import java.util.Date;
 @Setter
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Entity(name = "tb_pedido")
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private Long produto;
     private BigDecimal valor;
