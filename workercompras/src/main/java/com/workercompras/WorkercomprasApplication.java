dc3aca2034cc487be80f9507cfb5021b3332ae1d
lucasbarrossantos <lucas-barros28@hotmail.com>
2022-01-02 14:29:49 -0300

    Aula 14 - Buscando o endereco na API ViaCep com OpenFeign

--- workercompras/src/main/java/com/workercompras/WorkercomprasApplication.java
+++ workercompras/src/main/java/com/workercompras/WorkercomprasApplication.java
@@ -3,7 +3,9 @@ package com.workercompras;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableRabbit
 @SpringBootApplication
 public class WorkercomprasApplication {
