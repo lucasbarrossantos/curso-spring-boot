dc339ab7d7efd75e6366f0cc45db30405f8a4944
lucasbarrossantos <lucas-barros28@hotmail.com>
2022-01-02 22:38:12 -0300

    Aula 15 - Configuracao de log do FeignClient

--- workercompras/src/main/java/com/workercompras/config/FeignConfiguration.java
+++ workercompras/src/main/java/com/workercompras/config/FeignConfiguration.java
@@ -1,2 +1,15 @@
-package com.workercompras.config;public class FeignConfiguration {
+package com.workercompras.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.HEADERS;
+    }
+
 }
